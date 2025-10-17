43861575b005056874848a5d7329414f0c838a0b
michelelodi <mickey.lodi@gmail.com>
2025-10-17 15:49:04 +0200

    Initial Spring Boot project setup - Ready to grow some amazing features!

---
+++ account/src/main/java/it/gov/mgs/account/AccountApplication.java
@@ -0,0 +1,17 @@
+package it.gov.mgs.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Applicazione Spring Boot per la gestione degli account
+ * Progetto AI per il governo italiano
+ */
+@SpringBootApplication
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
+}
